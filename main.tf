88b9341fae95bbc8b4797a1c150261a2e444ee32
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-11 12:40:29 +0530

    azure github actions

--- main.tf
+++ main.tf
@@ -130,6 +130,7 @@ resource "null_resource" "jenkins" {
   provisioner "remote-exec" {
     inline = [
       "sudo dnf install -y git ansible-core",
+      "sudo ansible-galaxy collection install community.docker",
       "sudo dnf install python3.13-pip -y",
       "sudo pip3.11 install ansible",
       "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/azure-github-runner.git runner.yml -e TOKEN=${var.TOKEN}"
